8b5715abef8a69285e4fcfbcc51e34445857e2d4
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-26 22:54:48 +0800

    finished

--- joshuavasquezsalcedo/src/pages/index.js
+++ joshuavasquezsalcedo/src/pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import AboutMe from "@modules/component/about_me";
+import Body from "@modules/component/body";
 
 export default function Home() {
   return (
       <div className="current-theme">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Title</title>
+          <title>About me</title>
 
         </Head>
-          <div>
+          <div className="container mt-5">
         <AboutMe/>
+              <Body/>
         </div>
       </div>
   )
